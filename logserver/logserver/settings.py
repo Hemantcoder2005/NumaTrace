0f79f078233e5b9b2bc8c109c8dd37117107f77a
Hemantcoder2005 <hemantnarula2203@outlook.com>
2025-06-29 21:36:20 +0530

    fixed

--- logserver/logserver/settings.py
+++ logserver/logserver/settings.py
@@ -24,8 +24,8 @@ SECRET_KEY = 'django-insecure-_x&tk_*4rq7-qgqaxn7&%=3$im(6oft6c$l8gc5qmppwi=c9hb
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://numatrace.numatixsolutions.com']
+ALLOWED_HOSTS = ['numatrace.numatixsolutions.com', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://numatrace.numatixsolutions.com','localhost', '127.0.0.1']
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
